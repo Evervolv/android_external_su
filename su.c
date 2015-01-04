d7c6869a22c1160eafabd835752e3ca729445b87
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-04 03:07:14 +0000

    Remove hardwired uids
    We're built as part of the system, so use the private system header.
    Remove the outdated documentation too, it doesn't match this variant
    of su
    
    Change-Id: I26921c261fb5a7c473e7bdb1c8df5b4c887236a6

--- su.c
+++ su.c
@@ -33,6 +33,7 @@
 #include <sys/stat.h>
 #include <stdarg.h>
 #include <sys/types.h>
+#include <private/android_filesystem_config.h>
 
 #include "su.h"
 #include "utils.h"
@@ -40,33 +41,6 @@
 extern int is_daemon;
 extern int daemon_from_uid;
 extern int daemon_from_pid;
-
-unsigned get_shell_uid() {
-  struct passwd* ppwd = getpwnam("shell");
-  if (NULL == ppwd) {
-    return 2000;
-  }
-
-  return ppwd->pw_uid;
-}
-
-unsigned get_system_uid() {
-  struct passwd* ppwd = getpwnam("system");
-  if (NULL == ppwd) {
-    return 1000;
-  }
-
-  return ppwd->pw_uid;
-}
-
-unsigned get_radio_uid() {
-  struct passwd* ppwd = getpwnam("radio");
-  if (NULL == ppwd) {
-    return 1001;
-  }
-
-  return ppwd->pw_uid;
-}
 
 int fork_zero_fucks() {
     int pid = fork();
